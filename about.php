474e90aa1ca769aac1dd402d4406ca21c4dca875
bishan <toton333@yahoo.com>
2014-04-05 17:14:07 +0530

    About page
    Added about page describing our unique quality

---
+++ about.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>About Page</title>
+<meta charset="utf-8">
+
+</head>
+
+<body>
+  <h1>Why are you going to fall in love with us</h1>
+  <p> Our foods have only one side effect -- L O V E</p>
+
+</body>
+  
+
+</html>
