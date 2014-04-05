0997a2a0a1a7cdd0f1e733da04690985ba4180ab
bishan <toton333@yahoo.com>
2014-04-05 15:57:08 +0530

    Contact page
    Added contact page with email and phone

---
+++ contact.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Contact Page</title>
+<meta charset="utf-8">
+
+</head>
+
+<body>
+  <h1>Contact us</h1>
+  <p>Email</p>
+  <p>Phone number</p>
+
+</body>
+  
+
+</html>
